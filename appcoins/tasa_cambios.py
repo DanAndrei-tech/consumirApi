ed71ca5bf70fc9fd40f4996160f3900e29eb155a
DanAndrei-tech <detesan7@gmail.com>
2023-04-17 22:42:16 +0200

    ejercicios hechos en tasa_cambios

--- appcoins/tasa_cambios.py
+++ appcoins/tasa_cambios.py
@@ -1,8 +1,42 @@
 import requests
 
-#Invocar al metodo get con la url especifica
-r = requests.get('https://rest.coinapi.io/v1/exchangerate/BTC/EUR?apikey=C666B578-CE6A-40FD-B738-F249F7F5E26A')
+apikey = 'C666B578-CE6A-40FD-B738-F249F7F5E26A'
+#creo un input para cargar la moneda digital
+moneda_cripto = input("Ingrese una criptomoneda conocida: ").upper()
 
+while moneda_cripto != "" and moneda_cripto.isalpha() == True:
+    
+
+  #Invocar al metodo get con la url especifica
+  r = requests.get(f'https://rest.coinapi.io/v1/exchangerate/{moneda_cripto}/EUR?apikey={apikey}')
+
+  respuesta = r.json()
+  #ejercicio 1, como capturamos el rate
+
+
+
+  #ejercicio 2, como capturar el error
+  if r.status_code == 200:
+      print("{:,.2f}€".format(respuesta['rate']))
+      break
+  else:
+      print(respuesta['error'])
+
+  moneda_cripto = input("Ingrese una criptomoneda conocida: ").upper()
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 print("codigo http resupuesta",r.status_code)
 
 print("Cabecera",r.headers['content-type'])
@@ -13,4 +47,8 @@ print("Respuesta en string",r.text)
 print(type(r.text))
 
 print("Respuesta en json:",r.json())
-print(type(r.text))
+print(type(r.json()))
+
+'''
+
+
---
+++ pruebitas.py
@@ -0,0 +1,14 @@
+
+
+respuesta = {
+  "time": "2023-04-17T19:56:15.0000000Z",
+  "asset_id_base": "BTC",
+  "asset_id_quote": "EUR",
+  "rate": 27003.717361010671593941806046
+}
+
+print("Esto es time: ",respuesta['time'])
+print("Esto es base: ",respuesta['asset_id_base'])
+print("Esto es quote: ",respuesta['asset_id_quote'])
+print("Esto es rate: ",respuesta['rate'])
+
